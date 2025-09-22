2d36b72591402aa6644b71155aff88201cb7e5a9
neto <only.narciso042@gmail.com>
2025-09-22 20:51:08 -0300

    add fakes pro db

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use app\Models\Classroom;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence(3),
+            'email' => $this->faker->unique()->safeEmail(),
+            'classroom_id' => Classroom::factory()
+        ];
+    }
+}
